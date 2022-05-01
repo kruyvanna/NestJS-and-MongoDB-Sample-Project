8a577446783b30249bacf492d6109e25902d9e08
Kruy Vanna <kruyvanna@gmail.com>
2022-05-01 13:46:57 +0700

    [Owner] init module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { OwnersModule } from './owners/owners.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/cat-project'),
     CatsModule,
+    OwnersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
